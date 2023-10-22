158b91c2ad0e2b7dc0eca9fa0bdea9185190ca8f
AStefankiv <stefankif35@gmail.com>
2023-10-22 11:34:18 -0600

    Add assertEqual.js file

--- assertEqual.js
+++ assertEqual.js
@@ -3,3 +3,5 @@ const age = 25;
 console.assert(age >= 18, 'You must be 18 or older to access this content');
 
 // If the condition is false, it will log the error message
+
+console.log("Hello Andrew!")
