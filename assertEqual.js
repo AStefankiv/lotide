979ca473fc3f5e748e427e05eccd816a7fb05a79
AStefankiv <stefankif35@gmail.com>
2023-10-22 13:52:59 -0600

    Committed changes

--- assertEqual.js
+++ assertEqual.js
@@ -1,14 +1,11 @@
-// const age = 25;
-// console.assert(age >= 18, 'You must be 18 or older to access this content');
-
-// console.assert(1 === 1.1);
-
-
-// FUNCTION IMPLEMENTATION
-const sum = function(a, b) { 
-  return a + b;
-}
-
-// TEST CODE
-console.assert(sum(1, 2) === 3);
-console.assert(sum(1, 20) === 3); // bad / incorrect assertion, and we see it fail!
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
+assertEqual(1, 2);
