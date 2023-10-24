e5bb91e7043d229ca41a65e41c891566ef96a925
AStefankiv <stefankif35@gmail.com>
2023-10-23 21:32:15 -0600

    new

--- eqArrays.js
+++ eqArrays.js
@@ -27,3 +27,4 @@ console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]));// => true
 console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]));// => false
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]),false); // => should PASS
