cf91ed45d547c88669ae4bf0f48f87b1d4ecfcc1
AStefankiv <stefankif35@gmail.com>
2023-10-23 21:28:10 -0600

    Created eqArray.js file

--- tail.js
+++ tail.js
@@ -19,3 +19,7 @@ assertEqual(result[1], "Labs");
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words);
 assertEqual(words.length, 3);
+let singleElementArray = tail(["Hello"]);
+assertEqual(singleElementArray.length, 0);
+let emptyArray = tail([]);
+assertEqual(emptyArray.length, 0);
