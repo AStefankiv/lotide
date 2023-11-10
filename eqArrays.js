d06ddc6a8177cc13134a307e0d8c4e994eae774a
AStefankiv <stefankif35@gmail.com>
2023-11-09 18:57:44 -0700

    PPPPPPPPPPPP

--- eqArrays.js
+++ eqArrays.js
@@ -19,11 +19,4 @@ const eqArrays = function(array1, array2) {
   return true;
 };
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3]));// => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1]));// => false
-
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]));// => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]));// => false
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]),false); // => should PASS
+module.exports = eqArrays;
